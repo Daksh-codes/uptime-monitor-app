0a42a3487a071e5db05dfca5f56c94104b4e954c
Daksh Ramrakhyani <dakshramrakhyani34@gmail.com>
2026-05-05 10:12:40 +0530

    feat: add user CRUD and login controllers

--- server/src/controller/userController.ts
+++ server/src/controller/userController.ts
@@ -1,5 +1,7 @@
 import { prisma } from "../prisma-client";
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
 import * as bcrypt from "bcrypt";
 
 const getUser = async (req: Request, res: Response) => {
@@ -21,14 +23,12 @@ const getUser = async (req: Request, res: Response) => {
       },
     });
 
-    // handle no user 
+    // handle no user
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
-  
     return res.json(user);
-    
   } catch (error) {}
 };
 
@@ -75,5 +75,72 @@ const addUser = async (req: Request, res: Response) => {
     return res.status(500).json({
       message: "Something went wrong",
     });
+  }
+};
+
+const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id as string;
+
+    // validate id
+    if (!id) {
+      return res.status(400).json({ message: "Invalid user id" });
+    }
+
+    //delete user
+    await prisma.user.delete({
+      where: { id },
+    });
+
+    return res.status(200).json({
+      message: `User with id ${id} has been deleted`,
+    });
+  } catch (error: any) {
+    
+    // prisma generates this error code P2025 when record not found
+    if (error.code === "P2025") {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const passwordMatched = await bcrypt.compare(user.password, password);
+
+    if (!passwordMatched) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user.id }, // payload
+      process.env.JWT_SECRET as string,
+      { expiresIn: "7d" }
+    );
+
+
+    return res.json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+
+
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong" });
   }
 };
